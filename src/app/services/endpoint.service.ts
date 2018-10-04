f8064b8028c8ad8de77a78b2202dde597bb7a13e
Eduardo Sanz García <esanzgar@gmail.com>
2018-10-04 10:37:48 +0100

    refactor (#14): alternative implementation

--- src/app/services/endpoint.service.ts
+++ src/app/services/endpoint.service.ts
@@ -1,22 +1,37 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from "../../environments/environment";
-// import { retry } from 'rxjs/operators';
+import { Observable, AsyncSubject, throwError } from 'rxjs';
+import { retry, pluck, tap, catchError } from 'rxjs/operators';
+
+interface ApiResponse {
+  _links: {
+    [key: string]: {
+      href: string
+    }
+  }
+}
 
 @Injectable()
 export class EndpointService {
-  public cache = {};
+  private _cache = new AsyncSubject<ApiResponse>();
 
   constructor(private _http: HttpClient) {
-    // When we use the version 6 of RxJS, include a retry operator
-    this._http.get(environment.apiHost).subscribe(response => this.cache = response);
+    this._cache.pipe(
+      tap(data => this._cache.next(data)),
+      tap(_ => this._cache.complete()),
+      // TODO: anything else before we re-throw the error?
+      catchError(error => throwError(error)),
+    ).subscribe();
+
+    this._http.get<ApiResponse>(environment.apiHost).pipe(
+      retry(3),
+    ).subscribe(this._cache);
   }
 
-  public find(name: string): string | null{
-    if(this.cache[name]){
-      return this.cache[name]['href'];
-    }
-    return null;
+  public find(name: string): Observable<string | undefined> {
+    return this._cache.asObservable().pipe(
+        pluck('_links', name, 'href')
+    );
   }
-
 }
