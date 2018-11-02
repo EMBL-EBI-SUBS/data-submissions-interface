2621ff5d1f0f81c148d99033a3c350424050ea49
David Richardson <davidr@ebi.ac.uk>
2018-11-02 14:16:34 +0000

    correct authentication endpoint
    auth endpoint had an extra /, prevents token refresh from working. Also
    fixed some config errors (using dev instances instead of test/prod)

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -6,7 +6,7 @@
 export const environment = {
   production: false,
   apiHost: 'https://submission-dev.ebi.ac.uk/api/',
-  authenticationHost: 'https://explore.api.aai.ebi.ac.uk/',
+  authenticationHost: 'https://explore.api.aai.ebi.ac.uk',
   uploadEndpoint: 'https://submission-dev.ebi.ac.uk/files/',
   validationSchemaEndpoint: 'https://subs-json-schema-validator.herokuapp.com/validate'
 };
