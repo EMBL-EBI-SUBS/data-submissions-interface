2a46d0df6249066fbbd4242b03d4155100f35d3b
Karoly Erdos <karoly@ebi.ac.uk>
2019-01-15 15:39:57 +0000

    Remove hardcoded file upload endpoint and request it from the API root

--- src/app/services/file.service.ts
+++ src/app/services/file.service.ts
@@ -2,36 +2,43 @@ import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { RequestsService } from './requests.service';
 import { Injectable } from '@angular/core';
-import { flatMap, map } from 'rxjs/operators';
+import { flatMap, map, pluck } from 'rxjs/operators';
 
 @Injectable()
 export class FileService {
   requestService: RequestsService;
-  fileEndpoint = environment.uploadEndpoint;
+  fileEndpoint: any;
 
   constructor(private _http: HttpClient) {
     this.requestService = new RequestsService(_http);
+    this.fileEndpoint = this.getUploadEndpoint();
   }
 
-    deleteFile(fileHref) {
-        return this.requestService.delete(fileHref);
-    }
+  getUploadEndpoint() {
+    return this.requestService.get(environment.apiHost).pipe(
+      pluck('_links', 'tus-upload', 'href')
+    );
+  }
 
-    getActiveSubmissionsFiles(activeSubmission) {
-        const contentsLinks = activeSubmission['_links']['contents']['href'];
-        const response = this.requestService.get(contentsLinks).pipe(
-          map(res => {
-            return res['_links']['files']['href'];
-          }),
-          flatMap((filesUrl) => {
-            return this.requestService.get(filesUrl);
-          }),
-          map(res => {
-            return res;
-          })
-        );
+  deleteFile(fileHref) {
+      return this.requestService.delete(fileHref);
+  }
 
-        return response;
-      }
+  getActiveSubmissionsFiles(activeSubmission) {
+    const contentsLinks = activeSubmission['_links']['contents']['href'];
+    const response = this.requestService.get(contentsLinks).pipe(
+      map(res => {
+        return res['_links']['files']['href'];
+      }),
+      flatMap((filesUrl) => {
+        return this.requestService.get(filesUrl);
+      }),
+      map(res => {
+        return res;
+      })
+    );
+
+    return response;
+  }
 
 }
